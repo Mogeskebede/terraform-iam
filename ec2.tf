f75e39cf21b1788a625b1fdba15d54d966fb70c9
mo-at-590183757374 <mogeshailu005@gmail.com>
2026-04-07 14:12:41 -0400

    Adding EC2

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,3 @@
-
-# PROVIDERS
-
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "use2"
-  region = "us-east-2"
-}
-
-
 # DATA SOURCES (DEFAULT VPC + SUBNETS)
 
 
--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "use2"
+  region = "us-east-2"
 }
